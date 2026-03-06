fe92286e3b779b63bf1307b0cbc21ed0b123a571
daarani24 <daarani2007@gmail.com>
2026-03-06 22:35:13 +0530

    Practiced 26_Remove suplicates

---
+++ Leetcode-practice/Arrays/26_Remduplicates.py
@@ -0,0 +1,14 @@
+class Solution:
+    def removeDuplicates(self, nums):
+        n = len(nums)
+        k = 0
+        nums[k] = nums[0]
+        k += 1
+        for i in range(1, n):
+            if nums[i] != nums[i-1]:
+                nums[k] = nums[i]
+                k += 1
+        return k
+n=list(map(int,input().split()))
+for i in n:
+    print(removeDuplicates(i))
