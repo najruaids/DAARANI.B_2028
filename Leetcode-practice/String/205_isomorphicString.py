4ecca17b1a0a9acac839a8af6bac2bd05adc6f0f
daarani24 <daarani2007@gmail.com>
2026-03-12 22:11:52 +0530

    practiced isomorphic string

---
+++ Leetcode-practice/String/205_isomorphicString.py
@@ -0,0 +1,18 @@
+def iso(s,t):
+    map_s={}
+    map_t={}
+    for i in range(len(s)):
+        if s[i] in map_s:
+            if map_s[s[i]]!=t[i]:
+                return False
+        else:
+            map_s[s[i]]=t[i]
+        if t[i] in map_t:
+            if map_t[t[i]]!=s[i]:
+                return False
+        else:
+            map_t[t[i]]=s[i]
+    return True
+s=input()
+t=input()
+print(iso(s,t))
